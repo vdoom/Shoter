6ac0a92c54dcbfa3e2ff7f08a7b40601cd8d57fb
Volodymyr KUKSYNOK <vdoom.heretic@gmail.com>
2013-10-18 00:04:22 +0300

    bots

---
+++ TestShooter/Assets/bot_script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bot_script : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent { get { return GetComponent<NavMeshAgent>(); } }
+
+    private Vector3 destPos = Vector3.zero;
+    private GameObject destObject = null;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (!destObject)
+            destObject = GameObject.Find("Bot(Clone)");
+        
+        if (destObject)
+        { destPos = destObject.transform.position; }
+        else { destPos = transform.position; }
+        
+        navMeshAgent.SetDestination(destPos);
+    }
+}
